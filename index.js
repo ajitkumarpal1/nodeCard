f687505c1ba71a2189ae11310c502b041efba30a
ajitkumarpal1 <palajit29@gmail.com>
2023-06-03 21:09:13 +0530

    Add index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var server = express();
 
-
 var routes = require('./routes/routes')
 var mongoose = require('mongoose');
 const cors = require('cors')
@@ -9,8 +8,6 @@ require('dotenv').config();
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-console.log("aa>>",dbHost,dbUser,dbPassword);
-/* mongodb+srv://<username>:<password>@<cluster-hostname>/<database>?retryWrites=true&w=majority */
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/newDB`, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     checkDB = () => {
         console.log("DB connected");
@@ -18,19 +15,16 @@ mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/newDB`, { useN
         console.log("Error connecting to DB: " + err);
     });
 
-/* mongodb://localhost:27017 */
-
-
 server.use(cors());
 server.use(express.json());
 console.log("A1");
 
 server.use(routes);
 
-server.listen(3000, (error) => {
+server.listen(process.env.PORT || 3000, (error) => {
     if (error) {
         console.log("Error starting server: " + error);
     } else {
-        console.log("Server started on port : 3000");
+        console.log("Server started");
     }
 });
